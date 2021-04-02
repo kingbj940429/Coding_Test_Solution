35726aec35237d93d0ac091c026b88ceddea21c0
unknown <kingbj0429@gmail.com>
2021-04-02 13:37:37 +0900

    add : 10773, 11656, 11490

--- beak_joon/note.py
+++ beak_joon/note.py
@@ -1,6 +1,29 @@
 ''' 연습장 '''
 
+from math import gcd
+'''최대공약수'''
 
-arr = ['a','b','c','a','d']
-del(arr[:-1])
-print(arr)
+def gcd_custom(x, y):
+    while y:
+        x, y = y, x%y
+    return x
+
+print(gcd_custom(18, 24))
+print(gcd(18, 24))
+
+
+''' 최소공배수 '''
+
+def lcm(x, y):
+    return x*y // gcd(x,y)
+
+print(lcm(18,24))
+
+def lcms(arr):
+    while True:
+        arr.append(lcm(arr.pop(), arr.pop()))
+        if(len(arr)) == 1:
+            return arr[0]
+
+print(lcms([2,4,6,8]))
+
