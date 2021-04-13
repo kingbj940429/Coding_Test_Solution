347d49314a1700304c9767e16cc0adb00fad41d3
kingbj940429 <kingbj0429@gmail.com>
2021-04-13 23:02:09 +0900

    add : b_1235, 5_3, 5_4

--- test.py
+++ test.py
@@ -1,10 +1,38 @@
-from math import pow
-num = '3212'
-base = 5
+from collections import deque
 
-result = 0
-for index, val in enumerate(num[::-1]):
-    result += int(val) * (base**index)
+def dfs(graph, v, visited):
+    print(v , end = " ")
+    visited[v] = True
 
-print(result)
-print(int(num,base))
+    for i in graph[v]:
+        if(not visited[i]):
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end= " ")
+        for i in graph[v]:
+            if(not visited[i]):
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    []
+    ,[2,3,8]
+    ,[1,7]
+    ,[1,4,5]
+    ,[3,5]
+    ,[3,4]
+    ,[7]
+    ,[2,6,8]
+    ,[1,7]
+]
+
+visited = [False] * 9
+
+#dfs(graph, 1, visited)
+bfs(graph, 1, visited)
