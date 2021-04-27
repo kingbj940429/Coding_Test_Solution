8f4a0726b3ad26088ff699735155f1e0b61b0053
unknown <kingbj0429@gmail.com>
2021-04-27 17:15:52 +0900

    add : 1463, 1699

--- test.py
+++ test.py
@@ -1,21 +1,10 @@
-'''
-최대 길이는 10
-각 길이별 가격이 주어짐
-'''
-
-# 각 길이별 가격 (0부터 시작)
-p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-
-n = int(input())
-
-d = [0] * (n+1)
-
-for i in range(1,n+1):
-    temp = 0
-    for j in range(i+1):
-        temp = max(temp,d[i-j] + p[j])
-    d[i] = temp
-print(d[n]) 
+def solution(phone_book):
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
 
 
 
+print(solution(["12312312312", "97674223", "1195524421","97"]))
